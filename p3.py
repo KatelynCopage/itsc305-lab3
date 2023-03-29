c58bb0f83f4f39a0959d0d0ed9d5dae5b6172bce
KatelynCopage <93795698+KatelynCopage@users.noreply.github.com>
2023-03-29 17:58:17 -0600

    Create p3.py

---
+++ p3.py
@@ -0,0 +1,23 @@
+from gpiozero import LED, PWMOutputDevice
+from signal import pause
+#set frequency
+led = PWMOutputDevice(pin=17, active_high=True, initial_value=25, frequency=200):
+
+led.value = 0.25 #Set 25% duty cycle
+
+
+
+
+
+
+
+
+
+
+
+def pulse(self, fade_in_time = 3, fade_out_time = 3)
+	self.time
+	(
+		fade_in_time, fade_out_time
+	)
+pause()
